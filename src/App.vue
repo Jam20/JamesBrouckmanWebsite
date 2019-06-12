c507948e65c7c1e32bde91a1250efcafd00ca802
Jam20 <jbrouckman@gmail.com>
2019-06-12 15:58:42 -0400

    Started working on navbar
    created events that watch for hover
    NATIVE for router-link objects
    TODO
    change margin to padding
    change color when hovered

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app">
     <div id="Navbar">
-      <router-link to="Home">Home</router-link>
-      <router-link to="About">About</router-link>
-      <router-link to="Projects">Projects</router-link>
-      <router-link to="Contact">Contact</router-link>
-      
+      <router-link v-bind:class="{Hover:link1Hover}" @mouseenter.native="link1Hover=true" @mouseleave.native="link1Hover = false" to="Home">Home</router-link>
+      <router-link v-bind:class="{Hover:link2Hover}" @mouseenter.native="link2Hover=true" @mouseleave.native="link2Hover = false" to="About">About</router-link>
+      <router-link v-bind:class="{Hover:link3Hover}" @mouseenter.native="link3Hover=true" @mouseleave.native="link3Hover = false" to="Projects">Projects</router-link>
+      <router-link v-bind:class="{Hover:link4Hover}" @mouseenter.native="link4Hover=true" @mouseleave.native="link4Hover = false" to="Contact">Contact</router-link>
     </div>
     <router-view/>
   </div>
@@ -13,15 +12,37 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      link1Hover: false,
+      link2Hover: false,
+      link3Hover: false,
+      link4Hover: false,
+
+    }
+  }
+ 
 }
+
 </script>
 
 <style>
-#Navbar{
-  background-color: red
+@import url("Variables.css");
+
+#app{
+  background-color: var(--BackgroundPrimary);
 }
-#Navbar router-link{
-  background-color: black
+
+#Navbar a{
+  color: var(--BackgroundAccent);
+  font-size: 25pt;
+  text-decoration: none;
+  margin: 20px;
 }
+
+a.Hover{
+  background-color: #4f4f4f;
+}
+
 </style>
