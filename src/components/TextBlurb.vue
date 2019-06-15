34628c036222c3ab77502cf805c72ebb1c514a95
Jam20 <jbrouckman@gmail.com>
2019-06-14 23:36:45 -0400

    Worked on TextBlurb component and Home Page
    TextBlurb:
    -created props for input data
    -setup style sheet to refrense props
    -set defaults and types for props
    
    HomePage:
    -Created a name placement
    -added a textBlurb and styled it with props
    -styled the title and blurb to the location of the picture
    
    TODO
    -finish styling the textBlurb component
    -find Picture for background (Senior Pic)
    -Design Logo

--- src/components/TextBlurb.vue
+++ src/components/TextBlurb.vue
@@ -1,15 +1,57 @@
 <template>
-    <div id="BlurbBorder">
-
+    <div id="BlurbBorder" :style="BoxStyle">
+        <p>{{text}}</p>
     </div>
 </template>
 
 <script>
 export default {
     
+
+    
+    props: {
+        height: {
+            type: Number,
+            required: true,
+        },
+        width: {
+            type: Number,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: false,
+        },
+        text:{
+            type: String,
+            required: true,
+        },
+        backgroundColor: {
+            type: String,
+            default: "#ffffff",
+        }
+    },
+    data(){
+        return{
+          BoxStyle:{
+            backgroundColor: this.backgroundColor,
+            height: this.height+"px",
+            width: this.width+"px"
+        }
+        }
+    },
+    computed: {
+        
+    }
 }
 </script>
 
 <style scoped>
-    
+    #BlurbBorder{
+        border-radius: 20%
+    }
+    #BlurbBorder p{
+        margin-bottom: 2%;
+        margin-left: 5%
+    } 
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,20 @@
 <template>
     <div id="HomePage">
         <img id="BackgroundImgOne" src="https://via.placeholder.com/1920x750"/>
-        <textBlurb></textBlurb>
+        <div id="Title">
+            <h1>James Brouckman</h1>
+            <div id="line"/>
+            <h2>Computer Engineering Student</h2>
+        </div>
+        <textBlurb class="InfoBlurb" :height="300" :width="250" text="Hello World"></textBlurb>
+
     </div>
 </template>
 <script>
 import TextBlurb from "../components/TextBlurb"
 
 export default {
+
     components: {
         TextBlurb
     }    
@@ -17,5 +24,26 @@ export default {
     #HomePage{
         position: absolute;
         top: 0%;
+    }
+    .InfoBlurb{
+        position: absolute;
+        top:20%;
+        right:20%;
+    }
+    #Title{
+        position: absolute;
+        top: 40%;
+        left:20%;
+        font-family: -'Segoe UI';
+        color: white;
+    }
+    #Title h1{
+        font-size: 45pt;
+        margin-bottom: 2%;
+    }
+    #line{
+        width:100%;
+        height: 3px;
+        background-color: white;
     }
 </style>
