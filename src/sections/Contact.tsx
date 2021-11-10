ac89290c99e9fdf7a900cbb8dc5e6a7542efa523
James Brouckman <jbrouckman@gmail.com>
2021-11-10 12:06:57 -0500

    Began work on the contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,14 +1,22 @@
 import { Box } from '@material-ui/core'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { SocialIcon } from 'react-social-icons';
 import './Contact.scss'
 
 export default class Contact extends Component {
 
     render(): JSX.Element {
         return (
-            <Box>
-                Contact
+            <Box id = "ContactSection">
+                <div className= "IconSection"> 
+                    <SocialIcon className = "ContactIcon" id="LinkedInIcon" url="https://www.linkedin.com/in/james-brouckman/" />
+                    <SocialIcon className = "ContactIcon" id="GithubIcon"url="https://github.com/Jam20" />
+                </div>
+                <a href="tel:616-560-7930" id="email">jcbrouck@mtu.edu</a>
+                <a href="mailto:jcbrouck@mtu.edu" id="phone">(616)-560-7930</a>
+                
+
             </Box>
         )
     }
