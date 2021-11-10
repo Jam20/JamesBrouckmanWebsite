893894781cfc3c436e353a2d62aefd33b7cf4eb5
James Brouckman <jbrouckman@gmail.com>
2021-11-10 12:07:38 -0500

    swapped email and phone links

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -13,8 +13,8 @@ export default class Contact extends Component {
                     <SocialIcon className = "ContactIcon" id="LinkedInIcon" url="https://www.linkedin.com/in/james-brouckman/" />
                     <SocialIcon className = "ContactIcon" id="GithubIcon"url="https://github.com/Jam20" />
                 </div>
-                <a href="tel:616-560-7930" id="email">jcbrouck@mtu.edu</a>
-                <a href="mailto:jcbrouck@mtu.edu" id="phone">(616)-560-7930</a>
+                <a href="mailto:jcbrouck@mtu.edu" id="email">jcbrouck@mtu.edu</a>
+                <a href="tel:616-560-7930"  id="phone">(616)-560-7930</a>
                 
 
             </Box>
